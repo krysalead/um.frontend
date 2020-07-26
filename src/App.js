bcdf389fa6cada24871e809068867c765fa5b434
Olivier DAUMAS <odaumas@amadeus.com>
2020-07-26 18:31:54 +0200

    implement error handling

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React from "react";
 import "./App.css";
 import AddUser from "./components/AddUser";
 import UserList from "./components/UserList";
-import { AlertTitle, Alert } from "@material-ui/lab";
 import { AppContext, config } from "./Context";
+import ErrorBoundary from "./ErrorBoundary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">User Management</header>
-      <Alert severity="error">
-        <AlertTitle>Backend Error</AlertTitle>
-        There is an error
-      </Alert>
       <AppContext.Provider value={config}>
-        <AddUser />
-        <UserList />
+        <ErrorBoundary>
+          <AddUser />
+          <UserList />
+        </ErrorBoundary>
       </AppContext.Provider>
     </div>
   );
