d780caf829f60cb20ee17c6f7ee341197f07fe8c
Olivier DAUMAS <odaumas@amadeus.com>
2020-07-26 15:35:14 +0200

    List user from server

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,18 +1,39 @@
 import React from "react";
 import User from "./User";
 import TextField from "@material-ui/core/TextField";
+import { DefaultApi } from "../api/api";
 
-function UserList(props) {
-  let listUser = [];
-  if (props.users) {
-    listUser = props.users.map((user) => <User user={user} key={user.email} />);
+class UserList extends React.Component {
+  headers = { lastName: "Last Name", firstName: "First Name", email: "Email" };
+  constructor(props) {
+    super(props);
+    this.state = { userList: [] };
   }
-  return (
-    <div>
-      <TextField id="outlined-basic" label="Search..." variant="outlined" />
-      <div>{listUser}</div>
-    </div>
-  );
+  componentDidMount() {
+    new DefaultApi(null, "http://localhost:4000/v1")
+      .listUser()
+      .then((response) => {
+        if (response.status == 0) {
+          this.setState({ userList: response.data });
+        } else {
+          console.log(response.message);
+        }
+      });
+  }
+  render() {
+    const listUser = this.state.userList.map((user) => (
+      <User user={user} key={user.email} />
+    ));
+    return (
+      <div>
+        <TextField id="outlined-basic" label="Search..." variant="outlined" />
+        <div className="user-list-display">
+          <User user={this.headers} key={this.headers.email} />
+          {listUser}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default UserList;
